09632165a9a892aebcd20907894fabc200068865
Arsalan Khattak <me@khattak.dev>
2021-03-10 20:26:46 +0500

    refac: moved CRUD to separate class

---
+++ crud_basics/DBCon.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace crud_basics
+{
+    class DBCon
+    {
+
+        private SqlConnection sqlCon;
+        private SqlCommand sqlCmd;
+        private SqlDataAdapter sqlAdapter;
+        public DBCon(string conn)
+        {
+            sqlCon = new SqlConnection(conn);
+        }
+
+        public bool UDI(string query)
+        {
+            sqlCmd = new SqlCommand(query, sqlCon);
+            sqlCon.Open();
+            bool hasAffectedRow = sqlCmd.ExecuteNonQuery() > 0;
+            sqlCon.Close();
+
+            if (hasAffectedRow)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public DataTable Search(string query)
+        {
+
+            sqlCon.Open();
+            sqlAdapter = new SqlDataAdapter(query, sqlCon);
+            DataTable dt = new DataTable();
+
+            sqlAdapter.Fill(dt);
+            sqlCon.Close();
+
+            if (dt != null)
+            {
+                return dt;
+
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public DataSet View(string query)
+        {
+            sqlCon.Open();
+            sqlAdapter = new SqlDataAdapter(query, sqlCon);
+            DataSet ds = new DataSet();
+
+            sqlAdapter.Fill(ds, "Employees");
+            sqlCon.Close();
+
+            if (ds != null)
+            {
+                return ds;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
--- crud_basics/Form1.cs
+++ crud_basics/Form1.cs
@@ -13,6 +13,8 @@ namespace crud_basics
 {
     public partial class Form1 : Form
     {
+
+        DBCon conn = new DBCon("Data Source=DESKTOP-URS2I8O;Initial Catalog=crud;Integrated Security=True");
         public Form1()
         {
             InitializeComponent();
@@ -22,22 +24,16 @@ namespace crud_basics
         {
             string name = nameInput.Text;
             string email = emailInput.Text;
-            string conn = "Data Source=DESKTOP-URS2I8O;Initial Catalog=crud;Integrated Security=True";
-            SqlConnection sqlCon = new SqlConnection(conn);
-            string query = "INSERT INTO EMPLOYEES (NAME, EMAIL) VALUES('" + name + "','" + email + "')";
-            SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
-            sqlCon.Open();
-
-            if (sqlCmd.ExecuteNonQuery() > 0)
+            if (conn.UDI("INSERT INTO EMPLOYEES (NAME, EMAIL) VALUES('" + name + "','" + email + "')"))
             {
                 MessageBox.Show("Record Added!");
             }
             else
             {
                 MessageBox.Show("Unable to Add Record!");
+
             }
 
-            sqlCon.Close();
 
         }
 
@@ -45,20 +41,13 @@ namespace crud_basics
         {
 
             string name = nameInput.Text;
-            string conn = "Data Source=DESKTOP-URS2I8O;Initial Catalog=crud;Integrated Security=True";
-            SqlConnection sqlCon = new SqlConnection(conn);
-            string query = "SELECT * FROM EMPLOYEES WHERE NAME='" + name + "'";
-            sqlCon.Open();
-            SqlDataAdapter sqlAdapter = new SqlDataAdapter(query, sqlCon);
-            DataTable dt = new DataTable();
 
-            sqlAdapter.Fill(dt);
-            sqlCon.Close();
+            DataTable dt = conn.Search("SELECT * FROM EMPLOYEES WHERE NAME='" + name + "'");
 
             if (dt != null)
             {
 
-                nameInput.Text = dt.Rows[0]["Name"].ToString(); 
+                nameInput.Text = dt.Rows[0]["Name"].ToString();
                 emailInput.Text = dt.Rows[0]["Email"].ToString();
             }
             else
@@ -70,64 +59,41 @@ namespace crud_basics
 
         private void viewBtn_Click(object sender, EventArgs e)
         {
-            string name = nameInput.Text;
-            string conn = "Data Source=DESKTOP-URS2I8O;Initial Catalog=crud;Integrated Security=True";
-            SqlConnection sqlCon = new SqlConnection(conn);
-            string query = "SELECT * FROM EMPLOYEES";
-            sqlCon.Open();
-            SqlDataAdapter sqlAdapter = new SqlDataAdapter(query, sqlCon);
-            DataSet ds = new DataSet();
-
-            sqlAdapter.Fill(ds, "Employees");
-            recordGrid.DataSource = ds.Tables["Employees"].DefaultView;
-
-            sqlCon.Close();
-
             
+            DataSet ds = conn.View("SELECT * FROM EMPLOYEES");
+
+            recordGrid.DataSource = ds.Tables[0].DefaultView;
+
         }
 
         private void updateBtn_Click(object sender, EventArgs e)
         {
             string name = nameInput.Text;
             string email = emailInput.Text;
-            string conn = "Data Source=DESKTOP-URS2I8O;Initial Catalog=crud;Integrated Security=True";
-            SqlConnection sqlCon = new SqlConnection(conn);
-            string query = "UPDATE EMPLOYEES SET EMAIL='" + email + "' WHERE NAME='" + name + "'";
-            SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
-            sqlCon.Open();
-
-            if (sqlCmd.ExecuteNonQuery() > 0)
+            if (conn.UDI("UPDATE EMPLOYEES SET EMAIL='" + email + "' WHERE NAME='" + name + "'"))
             {
-                MessageBox.Show("Record Updated!");
+                MessageBox.Show("Record Added!");
             }
             else
             {
-                MessageBox.Show("Unable to Update Record!");
+                MessageBox.Show("Unable to Add Record!");
+
             }
 
-            sqlCon.Close();
         }
 
         private void deleteBtn_Click(object sender, EventArgs e)
         {
             string name = nameInput.Text;
-            string email = emailInput.Text;
-            string conn = "Data Source=DESKTOP-URS2I8O;Initial Catalog=crud;Integrated Security=True";
-            SqlConnection sqlCon = new SqlConnection(conn);
-            string query = "DELETE FROM EMPLOYEES WHERE NAME='" + name + "'";
-            SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
-            sqlCon.Open();
-
-            if (sqlCmd.ExecuteNonQuery() > 0)
+            if (conn.UDI("DELETE FROM EMPLOYEES WHERE NAME = '" + name + "'"))
             {
                 MessageBox.Show("Record Deleted!");
             }
             else
             {
                 MessageBox.Show("Unable to Delete Record!");
-            }
 
-            sqlCon.Close();
+            }
         }
     }
 }
